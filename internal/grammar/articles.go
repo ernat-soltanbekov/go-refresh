8a8083e8a51d21dcdec7d63f32aa5862057781c4
Yernat "GhostOfAstana" Soltanbekov <theastanadragon@gmail.com>
2026-07-29 13:56:32 +0500

    Add files via upload

--- internal/grammar/articles.go
+++ internal/grammar/articles.go
@@ -1,1 +1,15 @@
+package grammar
 
+import (
+	"regexp"
+	"strings"
+)
+
+func FixArticles(words []string) []string {
+	text := strings.Join(words, " ")
+	reLower := regexp.MustCompile(`\ba\s+([aeiouAEIOUhH])`)
+	text = reLower.ReplaceAllString(text, "an $1")
+	reUpper := regexp.MustCompile(`\bA\s+([aeiouAEIOUhH])`)
+	text = reUpper.ReplaceAllString(text, "An $1")
+	return strings.Fields(text)
+}
--- internal/grammar/articles_test.go
+++ internal/grammar/articles_test.go
@@ -1,1 +1,25 @@
+package grammar
 
+import (
+	"reflect"
+	"testing"
+)
+
+func TestFixArticles(t *testing.T) {
+	tests := []struct {
+		input    []string
+		expected []string
+	}{
+		{[]string{"a", "apple"}, []string{"an", "apple"}},
+		{[]string{"A", "orange"}, []string{"An", "orange"}},
+		{[]string{"a", "rock"}, []string{"a", "rock"}},
+		{[]string{"I", "found", "a", "amazing", "rock"}, []string{"I", "found", "an", "amazing", "rock"}},
+	}
+	for _, tc := range tests {
+		inputCopy := append([]string(nil), tc.input...)
+		result := FixArticles(inputCopy)
+		if !reflect.DeepEqual(result, tc.expected) {
+			t.Errorf("FixArticles(%v) = %v; ожидалось %v", tc.input, result, tc.expected)
+		}
+	}
+}
